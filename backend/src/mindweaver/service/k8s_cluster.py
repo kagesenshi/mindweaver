073fc1b71d962991d9b6554fd34a68f30327783f
Izhar Firdaus <kagesenshi.87@gmail.com>
2026-01-04 21:56:09 +0800

    fix enum warning

--- backend/src/mindweaver/service/k8s_cluster.py
+++ backend/src/mindweaver/service/k8s_cluster.py
@@ -2,18 +2,28 @@ import enum
 from typing import Optional
 
 from .base import ProjectScopedNamedBase, ProjectScopedService
-from sqlmodel import Field
-from sqlalchemy import String
+from sqlmodel import Field, Column
+from sqlalchemy import String, Enum as SQLEnum
 
 
-class K8sClusterType(str, enum.Enum):
+class K8sClusterType(enum.StrEnum):
     IN_CLUSTER = "in-cluster"
     REMOTE = "remote"
 
 
 class K8sCluster(ProjectScopedNamedBase, table=True):
     __tablename__ = "mw_k8s_cluster"
-    type: K8sClusterType = Field(default=K8sClusterType.REMOTE, sa_type=String)
+    type: K8sClusterType = Field(
+        sa_column=Column(
+            SQLEnum(
+                K8sClusterType,
+                native_enum=False,
+                values_callable=lambda x: [i.value for i in x],
+            ),
+            nullable=False,
+            server_default=K8sClusterType.REMOTE.value,
+        )
+    )
     kubeconfig: Optional[str] = Field(default=None, sa_type=String)
 
 
