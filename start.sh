c99fcaf1309e83fd8682bf7bb2d7327a94bab466
Izhar Firdaus <kagesenshi.87@gmail.com>
2026-02-21 14:29:36 +0800

    added initial migration

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start Uvicorn
 echo "Starting Uvicorn..."
-uvicorn mindweaver.app:app --host 0.0.0.0 --port 8000 &
+uv run uvicorn mindweaver.app:app --host 0.0.0.0 --port 8000 &
 
 # Start Nginx
 echo "Starting Nginx..."
