90de820427f32d43a97ad8ed06d2f332b86aa662
Izhar Firdaus <kagesenshi.87@gmail.com>
2026-05-22 08:16:25 +0800

    release ranger 0.1.2 (app: 2.8.0-rev.2)

--- images/ranger/ranger.sh
+++ images/ranger/ranger.sh
@@ -30,9 +30,9 @@ fi
 if [ "${SETUP_RANGER}" == "true" ]
 then
   cp ${RANGER_SCRIPTS}/ranger-admin-install.properties ${RANGER_HOME}/admin/install.properties
-  if [ -d ${RANGER_DEFAULT_CONF_DIR} ]
+  if [ -n "${RANGER_DEFAULT_CONF_DIR}" ] && [ -d "${RANGER_DEFAULT_CONF_DIR}" ]
   then
-      cp ${RANGER_DEFAULT_CONF_DIR}/* ${RANGER_HOME}/conf/
+      cp "${RANGER_DEFAULT_CONF_DIR}"/* "${RANGER_HOME}/conf/"
   fi
   {
           echo "db_flavor=${RANGER_DB_TYPE:-postgres}"
